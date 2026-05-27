936620befa0192e529864806122448d13d35bcee
Pavel Rusakov <149431538+pafthang@users.noreply.github.com>
2026-05-28 02:58:21 +0300

    Add SQLite database opener

---
+++ internal/db/db.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	"github.com/pafthang/paw/internal/config"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func Open() (*gorm.DB, error) {
+	if err := config.EnsureDir(); err != nil {
+		return nil, err
+	}
+	path, err := config.DBPath()
+	if err != nil {
+		return nil, err
+	}
+	database, err := gorm.Open(sqlite.Open(path), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	if err := database.AutoMigrate(&ChatSession{}, &ChatMessage{}, &MemoryItem{}); err != nil {
+		return nil, err
+	}
+	return database, nil
+}
