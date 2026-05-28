209e2046ed1ec5314de448a3c2c61f881a6c70c9
Pavel Rusakov <149431538+pafthang@users.noreply.github.com>
2026-05-28 03:05:53 +0300

    Add session persistence model tags

--- internal/db/models.go
+++ internal/db/models.go
@@ -3,11 +3,11 @@ package db
 import "time"
 
 type ChatSession struct {
-	ID        uint      `gorm:"primaryKey" json:"id"`
-	Title     string    `gorm:"index" json:"title"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	Messages  []ChatMessage
+	ID        uint          `gorm:"primaryKey" json:"id"`
+	Title     string        `gorm:"index" json:"title"`
+	CreatedAt time.Time     `json:"created_at"`
+	UpdatedAt time.Time     `json:"updated_at"`
+	Messages  []ChatMessage `json:"messages,omitempty"`
 }
 
 type ChatMessage struct {
